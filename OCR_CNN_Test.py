895a7688385c8c11030a7e99fe943da8dc9f2588
murtazahassan <mhpr@live.co.uk>
2020-02-20 19:14:18 +0300

    Update OCR_CNN_Test.py

--- OCR_CNN_Test.py
+++ OCR_CNN_Test.py
@@ -2,19 +2,23 @@ import numpy as np
 import cv2
 import pickle
 
-#####################################
+########### PARAMETERS ##############
 width = 640
 height = 480
-threshold = 0.65
+threshold = 0.65 # MINIMUM PROBABILITY TO CLASSIFY
+cameraNo = 1
 #####################################
 
-cap = cv2.VideoCapture(1)
+#### CREATE CAMERA OBJECT
+cap = cv2.VideoCapture(cameraNo)
 cap.set(3,width)
 cap.set(4,height)
 
+#### LOAD THE TRAINNED MODEL 
 pickle_in = open("model_trained.p","rb")
 model = pickle.load(pickle_in)
 
+#### PREPORCESSING FUNCTION
 def preProcessing(img):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img = cv2.equalizeHist(img)
@@ -28,7 +32,7 @@ while True:
     img = preProcessing(img)
     cv2.imshow("Processsed Image",img)
     img = img.reshape(1,32,32,1)
-    #Predict
+    #### PREDICT
     classIndex = int(model.predict_classes(img))
     #print(classIndex)
     predictions = model.predict(img)
